d2da45f0b0fa6593b26ac1c586d2d4649fdc15cc
danielkirwa <kirwadan03@gmail.com>
2020-07-23 00:40:42 +0300

    Initial commit

---
+++ src/princewheel/PrinceWheel.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package princewheel;
+
+/**
+ *
+ * @author owner
+ */
+public class PrinceWheel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      new Loader().setVisible(true);
+    }
+    
+}
